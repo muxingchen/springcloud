b392849344021a6397a9031e31db6c279e53cd4a
暮雨清风 <2856932721@qq.com>
2019-12-11 20:15:42 +0800

    eureka服务注册中心

--- src/main/java/com/yq/test/DoMain.java
+++ src/main/java/com/yq/test/DoMain.java
@@ -26,6 +26,11 @@ public class DoMain {
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	@Override
+	public String toString() {
+		return "DoMain [id=" + id + ", name=" + name + ", date=" + date + "]";
+	}
+	
 	
 	
 }
