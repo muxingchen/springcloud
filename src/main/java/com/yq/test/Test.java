6908d554e8cdecd03df5b9325a42ce32f2e0920f
暮雨清风 <2856932721@qq.com>
2019-12-12 10:25:08 +0800

    eureka服务端提交了

--- src/main/java/com/yq/test/Test.java
+++ src/main/java/com/yq/test/Test.java
@@ -4,8 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello对面的那个xxx");
-		System.out.println("Hello对面的那个xxx");
-		System.out.println("Hello对面的那个xxx");
 		
 	}
 
